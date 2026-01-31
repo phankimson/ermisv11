0c804755401b3491d89ac5ad20f384ab7184a391
Phan Kim Son <phankimson1988@gmail.com>
2026-01-31 14:54:09 +0700

    upp

--- public/mix/socket.js
+++ public/mix/socket.js
@@ -32939,7 +32939,6 @@ if (token) {
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'socket.io',
   host: window.location.hostname,
-  transports: ['websocket', 'polling'],
   secure: true,
   pingTimeout: 25000,
   upgradeTimeout: 21000,
